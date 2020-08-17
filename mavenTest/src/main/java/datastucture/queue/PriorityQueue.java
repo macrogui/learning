f846980977dd7622dc7720e39f80f338a5620474
macrogui <40064208+macrogui@users.noreply.github.com>
2020-08-17 23:03:47 +0800

    通过堆实现的优先队列，参考leetcode p347

---
+++ mavenTest/src/main/java/datastucture/queue/PriorityQueue.java
@@ -0,0 +1,41 @@
+package datastucture.queue;
+
+import datastucture.heap.MaxHeap;
+
+/**
+ * 优先队列
+ * @param <E>
+ */
+public class PriorityQueue<E extends Comparable<E>> implements Queue<E> {
+
+    private MaxHeap<E> maxHeap;
+
+    public PriorityQueue(){
+        maxHeap = new MaxHeap<>();
+    }
+
+    @Override
+    public int getSize(){
+        return maxHeap.size();
+    }
+
+    @Override
+    public boolean isEmpty(){
+        return maxHeap.isEmpty();
+    }
+
+    @Override
+    public E getFront(){
+        return maxHeap.findMax();
+    }
+
+    @Override
+    public void enqueue(E e){
+        maxHeap.add(e);
+    }
+
+    @Override
+    public E dequeue(){
+        return maxHeap.extractMax();
+    }
+}
